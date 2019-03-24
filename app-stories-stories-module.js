24140f5a567f1057ce9240f3eead4e68bee0421a
Travis CI User <travis@example.org>
2019-03-24 04:13:08 +0000

    Committed new Educate version

---
+++ app-stories-stories-module.js
@@ -0,0 +1,144 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app-stories-stories-module"],{
+
+/***/ "./src/app/stories/stories.component.html":
+/*!************************************************!*\
+  !*** ./src/app/stories/stories.component.html ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<!-- Content -->\n<section>\n    <!--<div markdown [src]=\"post\"></div>-->\n    <markdown [src]=\"post\"  ngPreserveWhitespaces></markdown>\n    <header class=\"main\">\n      <h1>Generic</h1>\n    </header>\n\n    <span class=\"image main\"><img src=\"./assets/images/pic11.jpg\" alt=\"\" /></span>\n\n    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>\n    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>\n    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>\n    \n    <hr />\n    <markdown [data]=\"postdata\"  ngPreserveWhitespaces></markdown>\n    <h2>Interdum sed dapibus</h2>\n    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>\n    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>\n\n    <hr class=\"major\" />\n\n    <h2>Magna etiam veroeros</h2>\n    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>\n    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>\n\n    <hr class=\"major\" />\n\n    <h2>Lorem aliquam bibendum</h2>\n    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>\n    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>\n\n  </section>\n\n"
+
+/***/ }),
+
+/***/ "./src/app/stories/stories.component.ts":
+/*!**********************************************!*\
+  !*** ./src/app/stories/stories.component.ts ***!
+  \**********************************************/
+/*! exports provided: StoriesComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StoriesComponent", function() { return StoriesComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var ngx_markdown__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ngx-markdown */ "./node_modules/ngx-markdown/fesm5/ngx-markdown.js");
+
+
+
+
+var StoriesComponent = /** @class */ (function () {
+    function StoriesComponent(route, markdownService) {
+        this.route = route;
+        this.markdownService = markdownService;
+    }
+    StoriesComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.sub = this.route.params.subscribe(function (params) {
+            _this.post = 'assets/blog/posts/' + params['id1'] + '.md';
+        });
+        this.markdownService.renderer.heading = function (text, level) {
+            var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+            return '<header class="main">' + '<h' + level + '>' + text + '</h' + level + '>' + '</header>';
+        };
+        this.markdownService.renderer.image = function (href, title, text) {
+            var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+            return '<span class="image main"> <img src="' + href + '" title="' + title + '" alt="' + text + '" /> </span>';
+        };
+        this.markdownService.getSource('assets/blog/posts/test3.md').subscribe(function (data) {
+            var x = data.split('---').slice(2, 3);
+            this.postdata = x[0];
+            console.log('DEBUG: RouteEvent Log area seven' + x[0]);
+        }.bind(this), function (errors) {
+            console.log('DEBUG:E: RouteEvent Log area eight', errors);
+            this.postdata = 'Error';
+        }.bind(this));
+    };
+    StoriesComponent.prototype.ngOnDestroy = function () {
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
+    };
+    StoriesComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-stories',
+            template: __webpack_require__(/*! ./stories.component.html */ "./src/app/stories/stories.component.html")
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_router__WEBPACK_IMPORTED_MODULE_2__["ActivatedRoute"], ngx_markdown__WEBPACK_IMPORTED_MODULE_3__["MarkdownService"]])
+    ], StoriesComponent);
+    return StoriesComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/stories/stories.module.ts":
+/*!*******************************************!*\
+  !*** ./src/app/stories/stories.module.ts ***!
+  \*******************************************/
+/*! exports provided: StoriesModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StoriesModule", function() { return StoriesModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var ngx_markdown__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ngx-markdown */ "./node_modules/ngx-markdown/fesm5/ngx-markdown.js");
+/* harmony import */ var _stories_routes__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./stories.routes */ "./src/app/stories/stories.routes.ts");
+/* harmony import */ var _stories_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./stories.component */ "./src/app/stories/stories.component.ts");
+
+
+
+
+
+
+
+var StoriesModule = /** @class */ (function () {
+    function StoriesModule() {
+    }
+    StoriesModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            declarations: [_stories_component__WEBPACK_IMPORTED_MODULE_6__["StoriesComponent"]],
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_3__["RouterModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_3__["RouterModule"].forChild(_stories_routes__WEBPACK_IMPORTED_MODULE_5__["STORIES_ROUTES"]),
+                ngx_markdown__WEBPACK_IMPORTED_MODULE_4__["MarkdownModule"].forChild()
+            ],
+            exports: [ngx_markdown__WEBPACK_IMPORTED_MODULE_4__["MarkdownModule"]]
+        })
+    ], StoriesModule);
+    return StoriesModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/stories/stories.routes.ts":
+/*!*******************************************!*\
+  !*** ./src/app/stories/stories.routes.ts ***!
+  \*******************************************/
+/*! exports provided: STORIES_ROUTES */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "STORIES_ROUTES", function() { return STORIES_ROUTES; });
+/* harmony import */ var _stories_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./stories.component */ "./src/app/stories/stories.component.ts");
+
+var STORIES_ROUTES = [
+    { path: '', component: _stories_component__WEBPACK_IMPORTED_MODULE_0__["StoriesComponent"] },
+];
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=app-stories-stories-module.js.map
